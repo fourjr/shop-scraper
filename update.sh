eb97eeaf6cd744eb77c7374161604f21b55f6273
Jia Rong <28086837+fourjr@users.noreply.github.com>
2026-07-21 00:15:34 +0800

    fix

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 cd shop-scraper
 git pull
-docker build -t drinks shop-scraper
+docker build -t drinks .
 docker run --name drinks --add-host=host.docker.internal:host-gateway -e POSTGRES_URI=postgres://drinks@host.docker.internal:5432/drinks -e DOCKER=1 drinks
 cd ..
